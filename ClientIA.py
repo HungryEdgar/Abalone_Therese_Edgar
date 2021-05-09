c2628a9c4457ec2c67440ce4bea568807e05e60d
theresenta <78798338+theresenta@users.noreply.github.com>
2021-05-09 04:41:17 +0200

    Update ClientIA.py

--- ClientIA.py
+++ ClientIA.py
@@ -1,1 +1,9 @@
+import cherrypy
 
+
+class Client():
+    #Définissons la route qui donne accès à des informations
+    @cherrypy.expose
+    @cherrypy.tools.json_in() 
+    @cherrypy.tools.json_out()
+    def 
